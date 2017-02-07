9768979b4d77dc39a2f3b998a2f18095c4b497aa
digora <migo_andres@yahoo.com>
2017-02-07 23:13:58 +0800

    Added permissions dialogue box for maps activity

--- app/src/main/java/hellow/mobapde/com/helloworld/MainActivity.java
+++ app/src/main/java/hellow/mobapde/com/helloworld/MainActivity.java
@@ -1,13 +1,25 @@
 package hellow.mobapde.com.helloworld;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.jar.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private static final int MY_PERMISSION_MAP_GRANTED = 1;
+    private boolean mapsGranted = false;
     Button btnStart;
 
     @Override
@@ -15,16 +27,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestPermission();
+
         btnStart = (Button) findViewById(R.id.btn_start);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mapIntent = new Intent();
                 mapIntent.setClass(getBaseContext(), MapsActivity.class);
-                startActivity(mapIntent);
+
+                if(mapsGranted){
+                    startActivity(mapIntent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Map Permission Required", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
     }
-    
+
+
+    private void requestPermission(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Log.d(TAG, "Explaining to user rationale for permission");
+
+                //Allows user to maps activity
+                mapsGranted = true;
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSION_MAP_GRANTED);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSION_MAP_GRANTED: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d(TAG, "Permission granted. Running Maps Activity");
+
+                } else {
+
+                    Log.d(TAG, "Permission not granted.");
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+
+    }
 }
