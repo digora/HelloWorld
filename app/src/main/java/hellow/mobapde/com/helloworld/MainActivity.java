27bd04ee325606cc3137aaee7d620fa654c162dc
digora <migo_andres@yahoo.com>
2017-02-07 22:52:55 +0800

    Added Activity intent to MapsActivivity

--- app/src/main/java/hellow/mobapde/com/helloworld/MainActivity.java
+++ app/src/main/java/hellow/mobapde/com/helloworld/MainActivity.java
@@ -1,13 +1,30 @@
 package hellow.mobapde.com.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent();
+                mapIntent.setClass(getBaseContext(), MapsActivity.class);
+                startActivity(mapIntent);
+            }
+        });
+
     }
+    
 }
