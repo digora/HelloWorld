8722039fb00a418d89c61ed4fa5076b3aab966d6
Patrick Tobias <patrick_tobias@outlook.com>
2017-03-06 10:08:46 +0800

    Include LatLng in Stop bean

--- app/src/main/java/hellow/mobapde/com/helloworld/Beans/Stop.java
+++ app/src/main/java/hellow/mobapde/com/helloworld/Beans/Stop.java
@@ -1,5 +1,7 @@
 package hellow.mobapde.com.helloworld.Beans;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by Patrick on 3/3/2017.
  */
@@ -7,7 +9,7 @@ package hellow.mobapde.com.helloworld.Beans;
 public class Stop {
     private String description;
     //picture
-    //coordinates
+    private LatLng latLng;
     //the circle thingy
 
 
@@ -21,5 +23,29 @@ public class Stop {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public double getLatitude() {
+        return latLng.latitude;
+    }
+
+    public double getLongitude() {
+        return latLng.longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        latLng = new LatLng(latitude, latLng.longitude);
+    }
+
+    public void setLongitude(double longitude) {
+        latLng = new LatLng(latLng.latitude, longitude);
+    }
+
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
     }
 }
