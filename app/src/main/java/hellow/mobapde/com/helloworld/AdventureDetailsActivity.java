4cc399ced6b2427636eb55d80a144a6bfd816875
Red Lips <migo_andres@yahoo.com>
2017-04-05 20:07:42 +0800

    Hello World 1.0
    -Map Fragment (not polished)

--- app/src/main/java/hellow/mobapde/com/helloworld/AdventureDetailsActivity.java
+++ app/src/main/java/hellow/mobapde/com/helloworld/AdventureDetailsActivity.java
@@ -12,31 +12,92 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.FileInputStream;
 
-public class AdventureDetailsActivity extends AppCompatActivity {
+public class AdventureDetailsActivity extends AppCompatActivity implements OnMapReadyCallback{
     Button btnGo;
     Button btnCancel;
     TextView tvAdventureViewedTitle;
     TextView tvAdventureViewedDetails;
     //ImageView ivAdventureViewedPicture;
 
+    GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adventure_details);
 
-        createMapFragment();
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_detail);
+        supportMapFragment.getMapAsync(this);
 
-
-    }
-
-    private void createMapFragment(){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.map_container, new MapDetailsFragment());
-        fragmentTransaction.commit();
+        createContentView();
     }
 
 
+    private void createContentView(){
+
+        Intent collectIntent = getIntent();
+
+        btnGo = (Button) findViewById(R.id.btn_go);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent mapIntent = new Intent(getBaseContext(), MapsActivity.class);
+                startActivity(mapIntent);*/
+                finish();
+            }
+        });
+
+        btnCancel = (Button) findViewById(R.id.btn_cancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        /* Hello */
+
+        tvAdventureViewedTitle = (TextView) findViewById(R.id.tv_adventure_viewed_title);
+        tvAdventureViewedTitle.setText(collectIntent.getStringExtra("aName"));
+
+        tvAdventureViewedDetails = (TextView) findViewById(R.id.tv_adventure_viewed_details);
+        tvAdventureViewedDetails.setText(collectIntent.getStringExtra("aDetails"));
+
+        /*ivAdventureViewedPicture = (ImageView) findViewById(R.id.iv_adventure_viewed_picture);
+        ivAdventureViewedPicture.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.app_icon));*/
+
+        /*Bitmap bmp = null;
+        String filename = collectIntent.getStringExtra("aPicture");
+        try{
+            FileInputStream fis = openFileInput(filename);
+            bmp = BitmapFactory.decodeStream(fis);
+            fis.close();
+
+            ivAdventureViewedPicture.setImageBitmap(bmp);
+        }catch(Exception e){
+            e.printStackTrace();
+        }*/
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
